3569fd6dd33b35cf81c3368db0b729c81a4a601b
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2022-11-09 18:09:09 -0300

    feat: linked pages on home and menu

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,45 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/trilha/ux" element={<Trail />} />
-          <Route path="/trilha/qa" element={<Trail />} />
-          <Route path="/trilha/dev" element={<Trail />} />
+          <Route
+            path="/trilha/ux"
+            element={
+              <Trail
+                title="Ux Design"
+                duration="5h 26min"
+                contentNumber="25"
+                expirationDate="24/10/2023"
+                description="Entenda as necessidades do seu usuário e construa experiências inspiradoras."
+                progress="20"
+              />
+            }
+          />
+          <Route
+            path="/trilha/qa"
+            element={
+              <Trail
+                title="Quality Assurance (QA)"
+                duration="5h 31min"
+                contentNumber="20"
+                expirationDate="24/10/2023"
+                description="Testando qualidade de aplicações e códigos"
+                progress="5"
+              />
+            }
+          />
+          <Route
+            path="/trilha/dev"
+            element={
+              <Trail
+                title="Desenvolvimento Full Stack"
+                duration="7h 18min"
+                contentNumber="35"
+                expirationDate="24/10/2023"
+                description="Programação e Desenvolvimento de aplicações completas usando as mais modernas tecnologias do mercado"
+                progress="75"
+              />
+            }
+          />
           <Route path="/visualizacao" element={<ContentVisualizer />} />
         </Routes>
       </Layout>
--- src/pages/Trail/Trail.jsx
+++ src/pages/Trail/Trail.jsx
@@ -1,7 +1,28 @@
 import styles from './Trail.module.css';
 
-const Trail = () => {
-  return <section className={styles.trailContent}></section>;
+const Trail = props => {
+  return (
+    <section className={styles.trailContent}>
+      <section>
+        <div>
+          <div>
+            <h1>{props.title}</h1>
+            <small>
+              {props.duration} - {props.contentNumber} conteúdos
+            </small>
+            <small>Expira em {props.expirationDate}</small>
+          </div>
+          <p>{props.description}</p>
+          <progress max="100" value={props.progress}></progress>{' '}
+          <span>
+            <small>{props.progress}%</small>
+          </span>
+          <button>começar agora</button>
+        </div>
+        <div></div>
+      </section>
+    </section>
+  );
 };
 
 export default Trail;
